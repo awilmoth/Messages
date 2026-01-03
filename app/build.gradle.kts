e50f58ff9717d535f48de1226da7f9a33a691aa6
Aaron Wilmoth <aaron.wilmoth@protonmail.com>
2026-01-03 14:39:25 -0800

    fix: Add src/main/java to kotlin.srcDirs for API compilation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ android {
     sourceSets {
         getByName("main") {
             java.srcDirs("src/main/java")
-            kotlin.srcDirs("src/main/kotlin")
+            kotlin.srcDirs("src/main/kotlin", "src/main/java")
         }
     }
 
